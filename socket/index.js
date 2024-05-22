5a89ad54cd1fdefec131146097252201ace090ff
hj1n <hjin8@korea.ac.kr>
2024-05-23 05:37:44 +0900

    feat: Socket init

---
+++ socket/index.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const app = express();
+const server = require("http").createServer(app);
+const socketIO = require("socket.io")(server);
+
+const port = process.env.PORT || 3001;
+
+server.listen(port, () => {
+  console.log("Socket IO server listening on port" + port);
+});
+
+// socket이 연결되었을 때
+socketIO.on("connection", (socket) => {
+  // success라는 이벤트가 발생했을 때
+  console.log("socket connected");
+  socket.emit("success", "socket connected");
+
+  socket.on("gameStart", () => {
+    console.log("gameStart");
+  });
+});
