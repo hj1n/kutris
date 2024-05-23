41e42296ee113ca55a0c5dade6622c803cc2df20
hj1n <hjin8@korea.ac.kr>
2024-05-23 18:42:14 +0900

    chore: withoutSSL wss

--- src/socket.js
+++ src/socket.js
@@ -3,5 +3,5 @@ import { io } from "socket.io-client";
 const isBrowser = typeof window !== "undefined";
 
 export const socket = isBrowser
-  ? io("http://192.168.68.57:3001", { transports: ["websocket"] })
+  ? io("https://kutrisserver.hjhj.kr:3001", { transports: ["websocket"] })
   : {};
