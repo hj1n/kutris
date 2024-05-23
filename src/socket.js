38d37f27ef0c1473b98cf55e16f78c5f75eadbe6
hj1n <hjin8@korea.ac.kr>
2024-05-23 21:07:05 +0900

    chore: socketIO with SSL

--- src/socket.js
+++ src/socket.js
@@ -3,5 +3,5 @@ import { io } from "socket.io-client";
 const isBrowser = typeof window !== "undefined";
 
 export const socket = isBrowser
-  ? io("https://kutrisserver.hjhj.kr:3001", { transports: ["websocket"] })
+  ? io("https://kutrisserver.hjhj.kr", { transports: ["websocket"] })
   : {};
