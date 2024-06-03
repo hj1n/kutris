3dfcb9ffeb6623c4a8fc26b4b7a28ada34667feb
hj1n <hjin8@korea.ac.kr>
2024-06-03 19:09:01 +0900

    bugfix: ret err

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -2,22 +2,23 @@ import "@/styles/globals.css";
 import Head from "next/head";
 
 export default function App({ Component, pageProps }) {
-  return;
-  <>
-    <Head>
-      <title>::: KUTRIS :::</title>
-      <meta name="title" property="og:title" content="::: KUTRIS :::" />
-      <meta
-        name="description"
-        property="og:description"
-        content="웹기반 테트리스 게임"
-      />
-      <meta
-        name="image"
-        property="og:image"
-        content="https://kutris.vercel.app/kutris.png"
-      />
-    </Head>
-    <Component {...pageProps} />
-  </>;
+  return (
+    <>
+      <Head>
+        <title>::: KUTRIS :::</title>
+        <meta name="title" property="og:title" content="::: KUTRIS :::" />
+        <meta
+          name="description"
+          property="og:description"
+          content="웹기반 테트리스 게임"
+        />
+        <meta
+          name="image"
+          property="og:image"
+          content="https://kutris.vercel.app/kutris.png"
+        />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
