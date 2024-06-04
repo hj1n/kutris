0e8411e33898b4b8baa81cf7990b7d51c030203a
hj1n <hjin8@korea.ac.kr>
2024-06-05 03:16:04 +0900

    chore: socketPing

--- socket/index.js
+++ socket/index.js
@@ -2,8 +2,8 @@ const express = require("express");
 const app = express();
 const server = require("http").createServer(app);
 const io = require("socket.io")(server, {
-  // pingInterval: 3000,
-  // pingTimeout: 5000,
+  pingInterval: 3000,
+  pingTimeout: 5000,
 });
 
 class Player {
